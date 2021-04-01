bcae24cad2cec85bb6ad3ce7c10e4af2949a8144
jeff-tilton <>
2021-03-31 19:07:25 -0700

    added flatten_dict

--- src/usaspending_client/utils.py
+++ src/usaspending_client/utils.py
@@ -14,3 +14,24 @@ def log_decorator(logger, level=10):
         return wrapper
 
     return real_decorator
+
+
+def flatten_dict(nested_dictionary):
+    "Flattens a nested dictionary"
+    res = {}
+
+    def get_all_values(nested_dictionary, parent_key):
+        # ref: https://www.kite.com/python/answers/how-to-loop-through-all-nested-dictionary-values-using-a-for-loop-in-python
+        for key, value in nested_dictionary.items():
+            if type(value) is dict:
+                if parent_key:
+                    k = f"{parent_key}__{key}"
+                else:
+                    k = key
+                get_all_values(value, parent_key=k)
+            else:
+                k = f"{parent_key}__{key}"
+                res.update({k: value})
+
+    get_all_values(nested_dictionary=nested_dictionary, parent_key="")
+    return res
